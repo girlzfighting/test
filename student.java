1ff221bf6fcb47cb4c61cb797d6ca5161887ec8d
dqxfc <2651727804@qq.com>
2020-12-03 21:12:03 +0800

    second commit

--- student.java
+++ student.java
@@ -2,4 +2,7 @@ import java.util.*
 class student{
   private string name;
   private string address;
+  private string email;   //修改的内容
+  private string phonenumber;//修改的内容
+  private string class;修改的内容
 }
