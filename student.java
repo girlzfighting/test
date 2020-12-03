791b783b7049946d485b3e10443ae26a56166af4
dqxfc <2651727804@qq.com>
2020-12-03 22:03:21 +0800

    four commit

--- student.java
+++ student.java
@@ -2,7 +2,8 @@ import java.util.*
 class student{
   private string name;
   private string address;
-  private string email;   //修改的内容
-  private string phonenumber;//修改的内容
-  private string class;修改的内容
+  private string email;   
+  private string phonenumber;
+  private string class;
+  private string sno; //新增学号属性
 }
